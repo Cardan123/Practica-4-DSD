26839fbf31066e904cca07529bb1374e3cf830e9
Carlos Daniel Villena Santiago <cosasdecardan@gmail.com>
2021-05-28 23:54:08 -0500

    Pruebas

--- ServerTiempo.py
+++ ServerTiempo.py
@@ -10,32 +10,38 @@ from datetime import datetime
 import sys
 import os
 
-host = '192.168.3.3'  #modify the ip addr as you need (server that gives the HOUR)
-port = 12345          #(MAIN SERVER, port that gives hour)
+host='192.168.1.64'  #modify the ip addr as you need (server that gives the HOUR)
+port=12345          #(MAIN SERVER, port that gives hour)
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock.connect((host,port))
-pause  = False
-factor = 1.0
+pause=False
+factor=1.0
 
-host4 = '192.168.3.3'  #modify the ip addr as you need 
-port4 = 12347          #(BACKUPSERVER, port that gives hour)
+host2='192.168.1.64'  #modify the ip addr as you need 
+port2=12346          #(MAIN SERVER, port that gives the BOOKS)
+sock2 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock2.connect((host2,port2))
+
+host3='192.168.1.64'  #modify the ip addr as you need 
+port3=12348          #(BACKUPSERVER, port that gives BOOKS)
+sock3 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock3.connect((host3,port3))
+
+host4='192.168.1.64'  #modify the ip addr as you need 
+port4=12347          #(BACKUPSERVER, port that gives hour)
 sock4 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock4.connect((host4,port4))
 
-def runClock(hour):
-    print("hola")
+host5='192.168.1.64'  #modify the ip addr as you need 
+port5=12349          #(BACKUPSERVER, port that listen the RESET BUTTON)
+sock5 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock5.connect((host5,port5))
 
-def receiveData():
-    print("hola")
-
-
-
-
-threadSend=threading.Thread(target=lambda: runClock(2))
-threadSend.start()
-
-threadReceive=threading.Thread(target=lambda: receiveData())
-threadReceive.start()
+host6='192.168.1.64'  #modify the ip addr as you need (server)
+port6=12341          #(MAINSERVER, port that listen the RESET BUTTON)
+sock6 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+sock6.connect((host6,port6))
 
 sock.close()
 sock4.close()
+
--- prac3/client.py
+++ prac3/client.py
@@ -7,34 +7,34 @@ from random import randint
 import socket
 import re
 
-host='192.168.3.3'  #modify the ip addr as you need (server that gives the HOUR)
+host='192.168.1.64'  #modify the ip addr as you need (server that gives the HOUR)
 port=12345          #(MAIN SERVER, port that gives hour)
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock.connect((host,port))
 pause=False
 factor=1.0
 
-host2='192.168.3.3'  #modify the ip addr as you need 
+host2='192.168.1.64'  #modify the ip addr as you need 
 port2=12346          #(MAIN SERVER, port that gives the BOOKS)
 sock2 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock2.connect((host2,port2))
 
-host3='192.168.3.3'  #modify the ip addr as you need 
+host3='192.168.1.64'  #modify the ip addr as you need 
 port3=12348          #(BACKUPSERVER, port that gives BOOKS)
 sock3 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock3.connect((host3,port3))
 
-host4='192.168.3.3'  #modify the ip addr as you need 
+host4='192.168.1.64'  #modify the ip addr as you need 
 port4=12347          #(BACKUPSERVER, port that gives hour)
 sock4 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock4.connect((host4,port4))
 
-host5='192.168.3.3'  #modify the ip addr as you need 
+host5='192.168.1.64'  #modify the ip addr as you need 
 port5=12349          #(BACKUPSERVER, port that listen the RESET BUTTON)
 sock5 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock5.connect((host5,port5))
 
-host6='192.168.3.3'  #modify the ip addr as you need (server)
+host6='192.168.1.64'  #modify the ip addr as you need (server)
 port6=12341          #(MAINSERVER, port that listen the RESET BUTTON)
 sock6 = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock6.connect((host6,port6))
