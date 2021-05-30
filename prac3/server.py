a2b900000fd94f0ee48280629b0109289e515042
Carlos Daniel Villena Santiago <cosasdecardan@gmail.com>
2021-05-30 00:50:34 -0500

    Prueba

--- prac3/server.py
+++ prac3/server.py
@@ -93,24 +93,6 @@ def runMasterClock():
             ':')[0]+':'+time_new.split(':')[1]+':'+str(int(time_new.split(':')[2])+1).zfill(2))
         
         txtVarClk0.set(time_new)
-        tiempo[0] = time_new
-        time_new = tiempo [1]
-        time_new = validateMasterHour(time_new.split(
-            ':')[0]+':'+time_new.split(':')[1]+':'+str(int(time_new.split(':')[2])+1).zfill(2))
-        
-        txtVarClk1.set(time_new)
-
-        time_new = tiempo [2]
-        time_new = validateMasterHour(time_new.split(
-            ':')[0]+':'+time_new.split(':')[1]+':'+str(int(time_new.split(':')[2])+1).zfill(2))
-        
-        txtVarClk2.set(time_new)
-
-        time_new = tiempo [3]
-        time_new = validateMasterHour(time_new.split(
-            ':')[0]+':'+time_new.split(':')[1]+':'+str(int(time_new.split(':')[2])+1).zfill(2))
-        
-        txtVarClk3.set(time_new)
         sleep(1*factor)
 
 
