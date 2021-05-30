5a95c140e392192fef9bca26e05a2c06e02e4120
Carlos Daniel Villena Santiago <cosasdecardan@gmail.com>
2021-05-30 02:17:03 -0500

    Prueba

--- prac3/server.py
+++ prac3/server.py
@@ -87,18 +87,19 @@ def validateMasterHour(hour):
 def runMasterClock():
     global parar1
     global tiempo
-    if parar1 == 0:
+    
         time_new = tiempo [0]
         global pause
         global factor
-        while pause == False:
-            time_new = validateMasterHour(time_new.split(
-                ':')[0]+':'+time_new.split(':')[1]+':'+str(int(time_new.split(':')[2])+1).zfill(2))
-            
-            txtVarClk0.set(time_new)
-            sleep(1*factor)
-    else:
-        break
+        if parar1 == 0:
+            while pause == False:
+                time_new = validateMasterHour(time_new.split(
+                    ':')[0]+':'+time_new.split(':')[1]+':'+str(int(time_new.split(':')[2])+1).zfill(2))
+                
+                txtVarClk0.set(time_new)
+                sleep(1*factor)
+        else:
+            break
 
 
 def sendBookInfo(connection):
