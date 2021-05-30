a34d3bb47d58e7cf8ff404e8d7d5238ef9e761ab
Carlos Daniel Villena Santiago <cosasdecardan@gmail.com>
2021-05-30 02:17:50 -0500

    Prueba

--- prac3/server.py
+++ prac3/server.py
@@ -93,11 +93,9 @@ def runMasterClock():
         global factor
         if parar1 == 0:
             while pause == False:
-                time_new = validateMasterHour(time_new.split(
-                    ':')[0]+':'+time_new.split(':')[1]+':'+str(int(time_new.split(':')[2])+1).zfill(2))
-                
-                txtVarClk0.set(time_new)
-                sleep(1*factor)
+                    time_new = validateMasterHour(time_new.split(':')[0]+':'+time_new.split(':')[1]+':'+str(int(time_new.split(':')[2])+1).zfill(2))
+                    txtVarClk0.set(time_new)
+                    sleep(1*factor)
         else:
             break
 
